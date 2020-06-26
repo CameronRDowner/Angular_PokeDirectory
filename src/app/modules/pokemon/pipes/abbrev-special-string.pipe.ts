a37feaa6e622076ca8f851674873f9f6cb19c102
Cameron Downer <camerondowner@gmail.com>
2020-06-26 18:52:47 -0500

    refactored abbrev-special-string pipe added pipes to select-box component option tag

--- src/app/modules/pokemon/pipes/abbrev-special-string.pipe.ts
+++ src/app/modules/pokemon/pipes/abbrev-special-string.pipe.ts
@@ -6,12 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AbbrevSpecialStringPipe implements PipeTransform {
 
   transform(value: string): string {
-    console.log(value)
     if(value.indexOf("special")!== -1){
       let splitString = value.slice(0).split("-")
-      console.log(splitString)
       splitString[0] = "Sp.";
-      splitString[1] = splitString[1].charAt(0).toUpperCase() + splitString[1].slice(1);
       return splitString.join(" ")
     }
     else{
