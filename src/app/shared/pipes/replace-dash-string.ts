9dcc192440a6bf22a7d4fa399d84ab5444753781
Cameron Downer <camerondowner@gmail.com>
2020-06-26 20:12:10 -0500

    fixed replace dash pipe not removing all occurances of dashes

--- src/app/shared/pipes/replace-dash-string.ts
+++ src/app/shared/pipes/replace-dash-string.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'replaceDash' })
 
 export class ReplaceDashPipe implements PipeTransform {
-    transform(value) {
-      return value.replace("-", " ");
+    transform(value : string): string {
+      return value.replace(/-/g, " ");
     }
   }
