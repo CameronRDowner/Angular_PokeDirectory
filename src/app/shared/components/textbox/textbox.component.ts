a6b2089b0fba5ee58ff17f4ce35632eedb8d2818
Cameron Downer <camerondowner@gmail.com>
2020-07-01 16:25:57 -0500

    added two-way binding to textbox component added clearButton functionality to textbox component

--- src/app/shared/components/textbox/textbox.component.ts
+++ src/app/shared/components/textbox/textbox.component.ts
@@ -9,12 +9,20 @@ export class TextboxComponent implements OnInit {
   @Input() length: string;
   @Input() clearButton: boolean; 
   @Output() textboxChange = new EventEmitter()
-  
+  textboxValue: string;
   emitChange(newText:string):void {
     this.textboxChange.emit(newText)
   }
+  handleChange():void{
+    this.emitChange(this.textboxValue);
+  }
+  initializeTextboxValue(){
+    console.log("ran")
+    this.textboxValue = ""
+  }
   constructor() {
-    this.clearButton = false
+    this.clearButton = false,
+    this.initializeTextboxValue();
    }
 
   ngOnInit(): void {
