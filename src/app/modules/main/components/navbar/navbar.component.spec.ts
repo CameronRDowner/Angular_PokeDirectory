95df141660f99c9e37600e81ed3a32bf87f8a9e6
Cameron Downer <camerondowner@gmail.com>
2020-08-04 18:56:56 -0500

    added dependecies to navbar component spec

--- src/app/modules/main/components/navbar/navbar.component.spec.ts
+++ src/app/modules/main/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +10,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, BrowserAnimationsModule],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
